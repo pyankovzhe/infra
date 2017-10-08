f523d161f31be31b354824dc85a23c363fe9ca36
Pyankov Evgeniy <pyankov.ev@gmail.com>
2017-10-08 20:16:06 +0300

    Add test checking if mongo db listening on 27017 port

--- ansible/roles/db/molecule/default/tests/test_default.py
+++ ansible/roles/db/molecule/default/tests/test_default.py
@@ -12,7 +12,12 @@ def test_mongo_running_and_enabled(host):
     assert mongo.is_enabled
 
 # check if configuration file contains the required line
-def test_config_file(File):
-    config_file = File('/etc/mongod.conf')
+def test_config_file(host):
+    config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
+
+# check if db listening 27017
+def test_db_27017_port(host):
+    socket = host.socket('tcp://0.0.0.0:27017')
+    assert socket.is_listening
